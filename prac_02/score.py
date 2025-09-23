2882ad6c2097252952f49322e6bdbd3b481b3db9
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-09-23 17:10:14 +1000

    Add score calculator

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,6 +3,7 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
+
 score = float(input("Enter score: "))
 if score < 0 or score > 100:
     print("Invalid score")
@@ -12,3 +13,4 @@ elif score >= 50:
     print("Passable")
 else:
     print("Bad")
+
