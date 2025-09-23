ba8f1c5c3d6de4adcfd5df7bc5b13b25243eb113
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-09-23 20:49:22 +1000

    Add score menu

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,21 +1,16 @@
 def main():
     minimum_length = 10
-
     password = get_password(minimum_length)
-
     print_asterisks(password)
-
 
 def print_asterisks(password: str):
     print("*" * len(password))
 
-
-def get_password(minimum_length: int) -> str:
+def get_password(minimum_length: int):
     password = input("Please enter a password:")
     while len(password) < minimum_length:
         print("Password is too short, try again")
         password = input("Please enter a password:")
     return password
-
 
 main()
--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -27,12 +27,12 @@ def main():
     print("Thank you.")
 
 
-def convert_fahrenheit_to_celsius(fahrenheit: float) -> float:
+def convert_fahrenheit_to_celsius(fahrenheit: float):
     """Convert Fahrenheit to Celsius."""
     return 5 / 9 * (fahrenheit - 32)
 
 
-def convert_celsius_to_fahrenheit(celsius: float) -> float:
+def convert_celsius_to_fahrenheit(celsius: float):
     """Convert Celsius to Fahrenheit."""
     return celsius * 9.0 / 5 + 32
 
