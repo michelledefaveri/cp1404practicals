3c67c768d6887d8463ee274012b82acc702928e3
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-09-26 21:43:38 +1000

    Add starting code provided for prac 2

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,40 +1,24 @@
 """
-CP1404 - Practical
-Convert temperature between degrees and fahrenheit
+CP1404/CP5632 - Practical
+Program for temperature conversion
 """
 
-def main():
-    """ Display a menu to the user, get input, and perform temperature conversions. """
-    menu = """ C - Convert Celsius to Fahrenheit \n F - Convert Fahrenheit to Celsius \n Q - Quit"""
-    print(menu)
-    # Get user choice
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print(f"Result: {fahrenheit:.2f} F")
+    elif choice == "F":
+        fahrenheit = float(input("Fahrenheit : "))
+        celsius = 5 / 9 * (fahrenheit - 32)
+        print(f"Result: {celsius:.2f} C")
+    else:
+        print("Invalid option")
+    print(MENU)
     choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = convert_celsius_to_fahrenheit(celsius)
-            print(f"Result: {fahrenheit:.2f} F")
-        elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = convert_fahrenheit_to_celsius(fahrenheit)
-            print(f"Result: {celsius:.2f} C")
-        else:
-            print("Invalid option")
-        print(menu)
-        choice = input(">>> ").upper()
-    print("Thank you")
-
-
-def convert_fahrenheit_to_celsius(fahrenheit):
-    """Convert Fahrenheit to Celsius."""
-    return 5 / 9 * (fahrenheit - 32)
-
-
-def convert_celsius_to_fahrenheit(celsius):
-    """Convert Celsius to Fahrenheit."""
-    return celsius * 9.0 / 5 + 32
-
-
-main()
-
-
+print("Thank you.")
