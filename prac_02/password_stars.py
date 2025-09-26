a5235bb28f226fc8a3218c39b7ee26e2e0d02ad1
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-09-26 16:36:42 +1000

    Add week two prac which prints a password as asterisks

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,13 +4,13 @@ Get a password and print the length as asterisks
 """
 
 def main():
-    """ Get a password and print as asterisks"""
+    """ Get a password and print as asterisks. """
     minimum_length = 10
     password = get_password(minimum_length)
     print_asterisks(password)
 
 def get_password(minimum_length):
-    """ Prompt user to enter a valid password"""
+    """ Prompt user to enter a valid password. """
     password = input("Please enter a password:")
     # Check password is greater than the minimum length
     while len(password) < minimum_length:
@@ -19,7 +19,7 @@ def get_password(minimum_length):
     return password
 
 def print_asterisks(password):
-    """Print a line of asterisks equal to the length of the password"""
+    """Print a line of asterisks equal to the length of the password. """
     # Print a line of asterisks equal to the password length
     print("*" * len(password))
 
