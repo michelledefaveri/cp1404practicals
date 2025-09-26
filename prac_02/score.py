631a4afa6d7e18dedd61acafcb5fdae7cfb9e052
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-09-26 21:29:21 +1000

    Move existing code into a main function

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,17 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main ():
+    """ Get a user score, print the result, and generate a random score."""
+    # Get user score
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main ()
