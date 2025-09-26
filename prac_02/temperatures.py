c89a7fba89ec4fb1c5303cd2554b5f53f9564fd0
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-09-26 21:55:04 +1000

    Add separate functions for conversions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -5,25 +5,33 @@ Program for temperature conversion
 
 def main():
     """ Display a menu to the user, get input, and perform temperature conversions """
-    menu = """C - Convert Celsius to Fahrenheit
-    F - Convert Fahrenheit to Celsius
-    Q - Quit"""
+    menu = """ C - Convert Celsius to Fahrenheit \n F - Convert Fahrenheit to Celsius \n Q - Quit"""
     print(menu)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit : "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
         print(menu)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+
+def convert_fahrenheit_to_celsius(fahrenheit: float) -> float:
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def convert_celsius_to_fahrenheit(celsius: float) -> float:
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
 
 
 main()
