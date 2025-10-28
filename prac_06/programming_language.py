dcd95397507ea553a51fdcfd34f5ef8eb3f358c0
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-10-28 20:55:31 +1000

    Add is_dynamic method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -7,7 +7,14 @@ class ProgrammingLanguage:
     """Represent a programming language with its properties."""
 
     def __init__(self, name, typing, reflection, year):
+        """Initialise a programming language object."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        """Return True if the program is dynamically typed or False if it is not"""
+        return self.typing.lower() == "dynamic"
+
+
