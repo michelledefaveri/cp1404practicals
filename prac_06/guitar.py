ca800d405b6af43b5ae635db315dd65b1dd1ec32
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-10-28 21:29:46 +1000

    Add guitar class and init function

---
+++ prac_06/guitar.py
@@ -0,0 +1,11 @@
+class Guitar:
+    """Guitar class for storing attributes of a guitar."""
+
+    def __init__(self, name="", year=0, cost=""):
+        """Initialise a guitar."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
+
