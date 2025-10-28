17f0dd31561aaaf981b6755d4368e8ed43261c6a
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-10-28 21:51:55 +1000

    Add function to run tests and create test guitars

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,8 @@
+from prac_06.guitar import Guitar
+
+def run_tests():
+    """Run test on Guitar class methods."""
+    # Create test guitars
+    guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    other = Guitar("Another Guitar", 2013, 1000.00)
+
