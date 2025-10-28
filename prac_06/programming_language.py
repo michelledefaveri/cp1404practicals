04db65d304d08b38477553f63cc9a56da08ed7a8
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-10-28 21:18:43 +1000

    Add string method to ProgrammingLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -13,6 +13,10 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        """Return a formatted string describing the language."""
+        return f"{self.name}, {self.typing} Typing, {self.reflection}=True, First appeared in {self.year}"
+
     def is_dynamic(self):
         """Return True if the program is dynamically typed or False if it is not"""
         return self.typing.lower() == "dynamic"
