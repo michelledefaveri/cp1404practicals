0965f55acb38688fc21e0907722ab545d7fed7c4
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-11-04 22:14:12 +1000

    add Project class

---
+++ prac_07/project.py
@@ -0,0 +1,14 @@
+"""
+Estimated time: 1 hour
+Actual time:
+"""
+
+from datetime import date
+
+class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
