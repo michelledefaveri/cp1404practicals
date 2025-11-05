0f8447285a2eebad987915e1d35b034cd82bd58d
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-11-05 19:35:20 +1000

    Add get_start_date function to class

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,21 +1,23 @@
-"""
-Estimated time: 1 hour
-Actual time:
-"""
-
 from datetime import date
 
 class Project:
-    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+    """Represents a single project."""
+
+    def __init__(self, name: str, start_date: date, priority: int, cost_estimate: float, completion: int):
         self.name = name
         self.start_date = start_date
         self.priority = priority
         self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
+        self.completion = completion
 
     def __str__(self):
-        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion}%"
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion}%")
 
     def is_complete(self):
-        """Return True if project is completed (100%)."""
-        return self.completion_percentage >= 100
+        return self.completion >= 100
+
+
+    def get_start_date(self):
+        return self.start_date
+
