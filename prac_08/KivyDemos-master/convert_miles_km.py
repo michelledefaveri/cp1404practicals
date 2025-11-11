35f2248d4dceec1b4c24729e84b2e12bcb093caf
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-11-11 18:33:25 +1000

    Add build method for kv layout

--- prac_08/KivyDemos-master/convert_miles_km.py
+++ prac_08/KivyDemos-master/convert_miles_km.py
@@ -1,3 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+MILES_TO_KM = 1.60934
+
+class MilesToKilometerConverterApp(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometres."
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
