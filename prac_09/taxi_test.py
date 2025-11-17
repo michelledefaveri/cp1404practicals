24492151ab2ac3711b382435dc9730ce8811a467
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-11-17 19:01:17 +1000

    Add main function for taxi tests

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,12 +1,12 @@
-from prac_09.taxi import Taxi
+from taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
 
-my_taxi.drive(40)
+def main():
+    """Test taxi class."""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    print(my_taxi)
 
-print(my_taxi)
-
-my_taxi.start_fare()
-
-print(my_taxi)
-
+main()
