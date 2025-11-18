50dc4d117566b1412fe659d26505ad9eee44272d
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-11-18 18:08:27 +1000

    Add test for SilverServiceTaxi

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,15 @@
+from silver_service_taxi import SilverServiceTaxi
+
+def main():
+    """Run tests for SilverServiceTaxi."""
+    taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
+
+    taxi.start_fare()
+    taxi.drive(18)
+    fare = taxi.get_fare()
+
+    print(f"Fare for an 18km trip with fanciness 2 is ${fare:.2f}")
+
+    assert fare == 48.78, f"Expected 48.78 and got {fare:.2f}"
+
+main()
