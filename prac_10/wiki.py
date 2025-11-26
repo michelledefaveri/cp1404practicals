e343f1a716fca097ce2ce557b2015bed2dcdbfff
michelledefaveri <michelle.defaveri@my.jcu.edu.au>
2025-11-26 11:49:42 +1000

    Add DisambiguationError and PageError exception handling

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,10 +3,18 @@ import wikipedia
 def main():
     title = input("Enter page title: ").strip()
     while title != "":
-        page = wikipedia.page(title, auto_suggest = False)
-        print(page.title)
-        print(page.summary[.500])
-        print(page.url)
+        try:
+            page = wikipedia.page(title, auto_suggest = False)
+            print(page.title)
+            print(page.summary[.500])
+            print(page.url)
+        except wikipedia.exceptions.DisambiguationError as e:
+            print("We need a more specific title. Try one of the following, or as a new search:")
+            print(e.options[:10])
+        except wikipedia.exceptions.PageError:
+            print(f'Page id "{title}" does not match any pages. Try another id!')
+
+
 
 
 
